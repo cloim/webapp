7b3291798fd6fca0c57d2e69ac99b602071ff836
cloim <cloimism@gmail.com>
2024-07-03 13:19:39 +0900

    theme 상태 스토어 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,30 @@
 <script>
+    import { onMount } from 'svelte';
     import { PUBLIC_APP_NAME } from '$env/static/public';
+    import { theme } from '$lib/store';
     import '../app.pcss';
+
+    onMount(() => {
+        const html = document.getElementsByTagName('html')[0];
+        const cur_theme = html.classList.contains('dark') ? 'dark' : 'light';
+        theme.set(cur_theme);
+
+        const observer = new MutationObserver((mutations) => {
+            const new_theme = html.classList.contains('dark') ? 'dark' : 'light';
+            theme.set(new_theme);
+        });
+
+        observer.observe(html, {
+            attributes: true,
+            attributeFilter: ['class'],
+            childList: false,
+            subtree: false
+        });
+    });
 </script>
 
 <svelte:head>
-  <title>{PUBLIC_APP_NAME}</title>
+    <title>{PUBLIC_APP_NAME}</title>
 </svelte:head>
 
 <slot />
