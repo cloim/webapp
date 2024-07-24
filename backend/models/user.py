ef4ede8812e9a9c3261ae9b7be872950d3cb8d6b
cloim <cloimism@gmail.com>
2024-07-24 17:24:46 +0900

    사용자 상태 추가

--- backend/models/user.py
+++ backend/models/user.py
@@ -9,3 +9,4 @@ class UserORM(Base):
     uid = Column(String(20), primary_key=True, index=True, comment="사용자 ID")
     uname = Column(String(20), nullable=False, comment="사용자 이름")
     upw = Column(String(100), nullable=False, comment="비밀번호")
+    status = Column(String(1), nullable=False, comment="계정 상태 (N: 정상, D: 삭제)")
